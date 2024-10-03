c180a2225fc8e4bdf44d1f059a3a93797f0d0521
Varsha Singh <varshasinghtx@gmail.com>
2024-10-03 12:17:51 -0400

    working

--- hw1/HW1final/main.c
+++ hw1/HW1final/main.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <string.h>
 #include <fcntl.h>
+#include <signal.h>
 
 #define MAX_LINE 512                                        // maximum number of lines from command line. use the size for malloc
 #define MAX_LENGTH_ARGS 32                                  // maximum number of letters in an argument
@@ -19,171 +20,94 @@
 // return child PID for waitpid in main
 // free memory for the_command struct(s) in main
 // int isFirst isLast, bool redirection flags
-int run_command(the_command* command, int input_fd, int isFirst, int isLast)
+void sighand()
 {
-    /*
-        command: pointer to the struct currently working with
-        input_fd: file descriptor for input, opened in main function
-        isFirst: flag for the first command in pipeline
-        isLast: flag for the last command in pipeline
-    */
     pid_t pid;
     int status;
-    int pipe_fd[2];     // created for connecting commands, sink and source. Not doing anything until dup2 connects ends, use this to connect commands with the fd?
-    int in_fd, out_fd, output_fd;  // realisticaly, only need one pipe for the entire pipeline, but need to connect the ends of the pipe to the commands
-    // and out is what I will pass
-    pipe(pipe_fd);
-
-    // check for redirect input
-    /*
-    // create the pipe if needed
-    if(!isLast)                             // if not the last command, need to make the pipe
+    while(TRUE)
     {
-        if(pipe(pipe_fd) == -1)
+        pid = waitpid(-1, &status, WNOHANG);
+        if(pid <= 0)
         {
+            break;
+        }
+    }
+    return;
+}
+int run_command(the_command* command, int input_fd, int isFirst, int isLast) {
+    pid_t pid;
+    int status;
+    int pipe_fd[2];     // Used for piping between commands
+    int in_fd, out_fd;  // Separate file descriptors for input/output redirection
+
+    // If not the last command, create a pipe for passing output to the next command
+    if (!isLast) {
+        if (pipe(pipe_fd) == -1) {
             perror("pipe");
             exit(EXIT_FAILURE);
         }
-        output_fd = pipe_fd[1];             // output goes to the write end of the pipe
-    }
-    else
-    {
-
-        output_fd = STDOUT_FILENO;
     }
 
-    if(command->redirect_output == NULL)
-    {
-        out_fd = STDOUT_FILENO;
-    }
-    else
-    {
-        out_fd = open(command->redirect_output, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-        if(out_fd == -1)
-        {
-            perror("open");
-            exit(EXIT_FAILURE);
-        }
-    }
-    */
+    // Fork the process
+    if ((pid = fork()) == 0) {  // Child process
 
-    // fork process, child and parent
-    if((pid = fork()) == 0)     // child process
-    {
-        // check for redirection of input and output
-        if(command->redirect_output)        // sets out_fd to the opened file
-        {
+        // Handle output redirection if specified
+        if (command->redirect_output) {
             out_fd = open(command->redirect_output, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-            if(out_fd == -1)
-            {
+            if (out_fd == -1) {
                 perror("open");
                 exit(EXIT_FAILURE);
             }
-        }
-        else
-        {
-            out_fd = STDOUT_FILENO;
-        }
-        if(out_fd != STDOUT_FILENO)         // if there is output redirection, dup2 to write port
-        {
-            pipe_fd[1] = out_fd;
-            if(dup2(out_fd, STDOUT_FILENO) == -1)
-            {
-                perror("OUT_FD DUP2");
-                exit(EXIT_FAILURE);
-            }
+            dup2(out_fd, STDOUT_FILENO);  // Redirect stdout to file
             close(out_fd);
-            close(pipe_fd[1]);
+        } else if (!isLast) {
+            // If it's not the last command, redirect output to the pipe
+            dup2(pipe_fd[1], STDOUT_FILENO);
         }
-        if(command->redirect_input)
-        {
+
+        // Handle input redirection if specified
+        if (command->redirect_input) {
             in_fd = open(command->redirect_input, O_RDONLY);
-            if(in_fd == -1)
-            {
+            if (in_fd == -1) {
                 perror("open");
                 exit(EXIT_FAILURE);
             }
-        }
-        else
-        {
-            in_fd = input_fd;
-        }
-        if(in_fd != STDIN_FILENO)
-        {
-            pipe_fd[0] = in_fd;
-            if(dup2(pipe_fd[0], STDIN_FILENO) == -1)
-            {
-                perror("IN_FD DUP2");
-                exit(EXIT_FAILURE);
-            }
+            dup2(in_fd, STDIN_FILENO);  // Redirect stdin to file
             close(in_fd);
-            close(pipe_fd[0]);
-        }
-
-        if(command->is_background)
-        {
-            close(STDOUT_FILENO);
-        }
-
-        // different cases for piping logic
-        // first and no input
-        if(isFirst && !isLast && command->redirect_input == NULL)
-        {
-            dup2(pipe_fd[1], STDOUT_FILENO);
-        }
-        else if(!isFirst && !isLast && input_fd != 0)
-        {
+        } else if (!isFirst) {
+            // If it's not the first command, input comes from the previous pipe
             dup2(input_fd, STDIN_FILENO);
-            dup2(pipe_fd[1], STDOUT_FILENO);
         }
-        else
-        {
-            if(command->redirect_input != NULL)
-            {
-                dup2(pipe_fd[0], STDOUT_FILENO);
-            }
-            if(command->redirect_output != NULL)
-            {
-                dup2(pipe_fd[1], STDIN_FILENO);
-            }
-            if(input_fd != 0)
-            {
-                dup2(input_fd, STDIN_FILENO);
-                close(input_fd);
-            }
-        }
-        if(execvp(command->command, command->arguments) == -1)
-        {
+
+        // Execute the command
+        if (execvp(command->command, command->arguments) == -1) {
             perror("execvp");
             exit(EXIT_FAILURE);
         }
 
-        close(pipe_fd[1]);
-        close(STDIN_FILENO);
-        dup(STDIN_FILENO);
-        close(pipe_fd[0]);
-    }
-    else
-    {
-        if(!command->is_background)
-        {
-            waitpid(pid, &status, 0);
+    } else {  // Parent process
+
+        if (!command->is_background) {
+            waitpid(pid, &status, 0);  // Wait for child process to finish
         }
+
+        // Close the write end of the pipe in the parent process
+        if (!isLast) {
+            close(pipe_fd[1]);
+        }
+
+        // Close input_fd from previous pipe if it’s not stdin
+        if (input_fd != STDIN_FILENO) {
+            close(input_fd);
+        }
+
+        // Return the read end of the pipe for the next command in the pipeline
+        return isLast ? 0 : pipe_fd[0];
     }
 
-    close(pipe_fd[1]);
+    return 0;
+}
 
-    if(input_fd != STDIN_FILENO)
-    {
-        close(input_fd);
-    }
-    if(command->next_command == NULL)
-    {
-        close(pipe_fd[0]);
-    }
-
-    return pipe_fd[0];
-};
 
 
 
@@ -204,7 +128,7 @@ int main(int argc, char* argv[])
             suppress = 1;
         }
     }
-
+    signal(SIGCHLD, sighand);
 
     while(TRUE)                                             // REPL: read, evaluate, print, loop; goes until CTRL+D ends it
     {
@@ -235,67 +159,21 @@ int main(int argc, char* argv[])
         the_command* head = parse_buffer(token_array);      // creates linked list of commands
         the_command* current = head;                        // first command in LL format
 
-        // create components for redirection
         int input_fd = STDIN_FILENO;                        // input fd
         int output_fd = STDOUT_FILENO;                      // output fd
-        int pipe_fd[2];                                     // pipe fd
-        pid_t pid;                                          // processid for fork
-        int child_status;                                   // signal
-
-
-        while(current != NULL)                              // for each command struct
+        while(current->next_command != NULL)                // for each command struct
         {
-            if(current->redirect_input != NULL && current == head)     // input redirection for first command is needed AND need to init input_fd
-            {
-                input_fd = open(current->redirect_input, O_RDONLY);     // open the file for reading
-                if(input_fd == -1)
-                {
-                    perror("open");
-                    exit(EXIT_FAILURE);
-                }
-            }
+            input_fd = run_command(current, input_fd, (current == head), (current->next_command == NULL));     // run the command
+            current = current->next_command;                // move to the next command
+        }
 
-            run_command(current, input_fd, (current == head), (current->next_command == NULL));     // run the command
+        input_fd = run_command(current, input_fd, (current == head), (current->next_command == NULL));     // run the command
 
-            // INPUT
-            /*
-                open specified input file
-                close STDIN
-                dup2 to redirect STDIN to input
-                close fd after redirection
-            */
-
-           // OUTPUT
-           /*
-                open w/ flags
-                close STDOUT
-                dup2 direct STDOUT to output
-                close fd after redirection
-           */
-          // PIPE
-          /*
-            create a pipe
-            fork to exec 1st command
-            redirect STDOUT to write end of pipe: dup2
-            fork second for chained process
-            redirect STDIN to read end of pipe: dup2
-            close both ends in parent after fork
-          */
-         /*
-            always create the pipe: only redirect in fd if redirection needed!
-            if in background: close fd[1] OUT
-            if first command: oly dup2 fd[1] OUT
-            if last command: only dup2 fd[0] IN
-            if in middle: dup2 both IN and OUT; input in IN and fd[1] OUT
-         */
-                current = current->next_command;
-            }
-                free_struct(head);
-                fflush(stdout);
+        free_struct(head);
+        fflush(stdout);
     }
     free(buffer);
     return 0;
-
 }
 
 
